0a01edc529ec8113e628fd12afa12acde729de43
pjudge@progress.com <pjudge@progress.com@7a97b29e-881c-e1fb-1594-d0cafe41bae1>
2011-01-10 22:17:42 +0000

    Refactor AutoEdgeTheFactory application code into client, server and shared parts/projects. This allows more granular control over deployment and configuration options.

---
+++ autoedgethefactory/server/cfg/runbproxygen.bat
@@ -0,0 +1,2 @@
+REM Command to run batch Proxy Generation
+"C:\devarea\dlc\oe102b\bin\bproxygen.bat" -xpxgfile="C:\devarea\projects\OEBP\autoedgethefactory\cfg\AutoEdgeTheFactoryBuildWebservices.xpxg" -startdir="c:\devarea\temp\oe102b"
