3ecf037d740081b1c112033eb0666b01c4b029da
pjudge@progress.com <pjudge@progress.com@7a97b29e-881c-e1fb-1594-d0cafe41bae1>
2011-08-18 14:06:02 +0000

    Include database dump (.d files) to package.

---
+++ trunk/autoedgethefactory/server/db/dump/_sec-authentication-domain.d
@@ -0,0 +1,21 @@
+"chery" "TABLE-ApplicationUser" "Authentication Domain for the chery tenant" "04130d0b17681223203e262433313a3c3e073c22207f696b3167787f607d327e61612e707f773732337f2a7763747e3060622a71637c676267342d" "" "" yes "" "" "" "" ""
+"fjord" "TABLE-ApplicationUser" "Authentication Domain for the fjord tenant" "04130d0b17681223203e262433313a3c3e073c22207f696b3167787f607d327e61612e707f773732337f2a7763747e3060622a72347c676267342d" "" "" yes "" "" "" "" ""
+"genericmotors" "TABLE-ApplicationUser" "Authentication Domain for the genericmotors tenant" "04130d0b17681223203e262433313a3c3e073c22207f696b3167787f607d327e61612e707f773732337f2a7763747e3060622a72337c676267342d" "" "" yes "" "" "" "" ""
+"h2omotors" "TABLE-ApplicationUser" "Authentication Domain for the h2omotors tenant" "04130d0b17681223203e262433313a3c3e073c22207f696b3167787f607d327e61612e707f773732337f2a7763747e3060622a72307c676267342d" "" "" yes "" "" "" "" ""
+"hinda" "TABLE-ApplicationUser" "Authentication Domain for the hinda tenant" "04130d0b17681223203e262433313a3c3e073c22207f696b3167787f607d327e61612e707f773732337f2a7763747e3060622a72317c676267342d" "" "" yes "" "" "" "" ""
+"motorgroupeast" "TABLE-ApplicationUser" "Authentication Domain for the motorgroupeast tenant" "04130d0b17681223203e262433313a3c3e073c22207f693660622a266027617e67347b767f71356b327f2a7763747e30676077256b726062676378" "" "" yes "" "" "" "" ""
+"potomoc" "TABLE-ApplicationUser" "Authentication Domain for the potomoc tenant" "04130d0b17681223203e262433313a3c3e073c22207f696b3167787f607d327e61612e707f773732337f2a7763747e3060622a71627c676267342d" "" "" yes "" "" "" "" ""
+"scubaroo" "TABLE-ApplicationUser" "Authentication Domain for the scubaroo tenant" "04130d0b17681223203e262433313a3c3e073c22207f696b3167787f607d327e61612e707f773732337f2a7763747e3060622a72377c676267342d" "" "" yes "" "" "" "" ""
+"toyola" "TABLE-ApplicationUser" "Authentication Domain for the toyola tenant" "04130d0b17681223203e262433313a3c3e073c22207f696b3167787f607d327e61612e707f773732337f2a7763747e3060622a72367c676267342d" "" "" yes "" "" "" "" ""
+.
+PSC
+filename=_sec-authentication-domain
+records=0000000000009
+ldbname=aetf
+timestamp=2011/08/18-09:52:58
+numformat=44,46
+dateformat=mdy-1950
+map=NO-MAP
+cpstream=UTF-8
+.
+0000002073
