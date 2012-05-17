2338d0ef814d4b8d86dbbd582a76ba5f2a5e39d3
pjudge@progress.com <pjudge@progress.com@7a97b29e-881c-e1fb-1594-d0cafe41bae1>
2012-05-17 15:09:01 +0000

    Remove branches/personal branch after moving to simple/trunk (and renaming to simple).

--- branches/personal/trunk/autoedgethefactory/server/cfg/runbproxygen.bat
+++
@@ -1,2 +0,0 @@
-REM Command to run batch Proxy Generation
-"C:\Devarea\dlc\oe102b\bin\bproxygen.bat" -xpxgfile="C:\devarea\projects\OEBP\autoedgethefactory\server\cfg\AutoEdgeTheFactoryWebservices.xpxg" -startdir="c:\devarea\temp\oe102b"
